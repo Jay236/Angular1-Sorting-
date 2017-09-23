8c8f36eea335f64fb4c9de7bf8cc83a1b8fb70ed
Jay236 <jaychander39@gmail.com>
2017-09-22 22:59:18 -0400

    Sorting order

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http){
+  
+    $http.get('data.json')
+    .then(function(response){
+    $scope.myData=response.data;
+
+        
+    });
+  
+});
+    
+    
+ 
+        
